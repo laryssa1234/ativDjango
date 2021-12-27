2ca032e79bd26282348e8e7cee7371f25f2a5e3b
laryssa1234 <94841549+laryssa1234@users.noreply.github.com>
2021-12-27 11:34:30 -0300

    commit 4

--- projeto1/mysite/polls/urls.py
+++ projeto1/mysite/polls/urls.py
@@ -8,8 +8,16 @@ urlpatterns = [
 
 from django.urls import path
 
+
 from . import views
 
+app_name = 'polls'
+urlpatterns = [
+    path('', views.indexView.as_view(), name='index'),
+    path('<int:pk>/', views.detailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.resultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
 urlpatterns = [
     # ex: /polls/
     path('', views.index, name='index'),
@@ -19,4 +27,6 @@ urlpatterns = [
     path('<int:question_id>/results/', views.results, name='results'),
     # ex: /polls/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
+
+    path('specifics/<int:question_id>/', views.detail, name='detail'),
 ]
